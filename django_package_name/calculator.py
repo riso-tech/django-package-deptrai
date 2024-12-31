81f1dccbfe4900cc87f7b65c995f3da2857f8610
Bin Nguyen <tu.nguyen@risotech.vn>
2024-12-31 14:57:04 +0000

    add sample code

---
+++ django_package_name/calculator.py
@@ -0,0 +1,18 @@
+class Calculator:
+    @staticmethod
+    def add(first_arg, second_arg):
+        return first_arg + second_arg
+
+    @staticmethod
+    def subtract(first_arg, second_arg):
+        return first_arg - second_arg
+
+    @staticmethod
+    def multiply(first_arg, second_arg):
+        return first_arg * second_arg
+
+    @staticmethod
+    def divide(first_arg, second_arg):
+        if second_arg == 0:
+            return "Cannot divide by 0 4"
+        return first_arg * 1.0 / second_arg
