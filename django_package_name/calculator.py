8adf9cbef79cd9295a068d4eacc03d659c3c3b9a
Bin Nguyen <tu.nguyen@risotech.vn>
2025-01-01 14:35:08 +0000

    test

--- django_package_name/calculator.py
+++ django_package_name/calculator.py
@@ -14,5 +14,5 @@ class Calculator:
     @staticmethod
     def divide(first_arg, second_arg):
         if second_arg == 0:
-            return "Cannot divide by 0 35"
+            return "Cannot divide by 0 36"
         return first_arg * 1.0 / second_arg
