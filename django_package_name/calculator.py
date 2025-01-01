7c8baf1d7c396f5ffaf7619176915596cec85e8c
Riso Tech Co.,Ltd. <git@risotech.vn>
2025-01-01 13:11:11 +0700

    Update calculator.py

--- django_package_name/calculator.py
+++ django_package_name/calculator.py
@@ -14,5 +14,5 @@ class Calculator:
     @staticmethod
     def divide(first_arg, second_arg):
         if second_arg == 0:
-            return "Cannot divide by 0 13"
+            return "Cannot divide by 0 14"
         return first_arg * 1.0 / second_arg
